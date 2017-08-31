6453fef3e664691eab4934ceb790b46ac327751b
PardeepS2 <Pardeemaloya@gmail.com>
2017-08-31 19:13:45 -0400

    create a simple layout with textview and button. Also create onclick method to get string.

--- app/src/main/java/com/pardeep/getstarted/MainActivity.java
+++ app/src/main/java/com/pardeep/getstarted/MainActivity.java
@@ -2,12 +2,33 @@ package com.pardeep.getstarted;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare our view variables
+
+    private TextView mTextView;
+    private Button mButtonView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign values to variables.
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButtonView = (Button) findViewById(R.id.button);
+        View.OnClickListener listner = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact = "ostriches are run faster then horses.";
+                mTextView.setText(fact);
+            }
+        };
+
+        mButtonView.setOnClickListener(listner);
     }
 }
